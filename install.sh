6b53d5ff320e2d6d4ebf47cf9a75f66dea8011c8
Jan Schulte <jan.schulte@asquera.de>
2016-02-29 09:17:01 +0100

    Install current ruby

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 
 ES_URL="https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.2.0/elasticsearch-2.2.0.deb"
 
+add-apt-repository ppa:brightbox/ruby-ng
+
 apt-get update
 
 #Install Java
-apt-get install -y openjdk-7-jre vim git
+apt-get install -y openjdk-7-jre vim git curl build-essential ruby2.2 ruby2.2-dev
+gem install bundler
 
 #Download ES .deb package
 wget $ES_URL
