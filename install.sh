43abf25e1ed1cf9924b62efac0eaeae98249eac7
Jan Schulte <jan.schulte@asquera.de>
2016-02-29 08:51:20 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+ES_URL="https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.2.0/elasticsearch-2.2.0.deb"
+
+apt-get update
+
+#Install Java
+apt-get install -y openjdk-7-jre
+
+#Download ES .deb package
+wget $ES_URL
+
+#Install package
+dpkg -i elasticsearch-2.2.0.deb
+
+#Start ES automatically
+update-rc.d elasticsearch defaults
